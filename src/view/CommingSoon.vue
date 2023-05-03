c85c1759a9eb0078343bedb84edc86bc100cd157
My Duyen <duyenb1910046@student.ctu.edu.vn>
2023-05-03 20:46:45 +0700

    WEB

---
+++ src/view/CommingSoon.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="page-single-2">
+        <div class="container">
+            <div class="row ipad-width">
+                <div class="left-content">
+                    <a href="index-2.html"><img class="md-logo" src="@/assets/images/logoweb.jpg" alt="" style="height: 100px; width: 180px;"></a>
+                    <h1>Coming soon</h1>
+                    <p>We are working hard to get back to you in</p>
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12 col-xs-12">
+                            <div class="coming-ct">
+                                <div id="clockdiv" class="time">
+                                    <div class="it-time">
+                                        <span class="days"></span><br>
+                                        <p>45 Seconds</p>
+                                    </div>
+                                    
+                                    <div class="it-time">
+                                        <span class="hours"></span><br>		
+                                        <p>60 Minutes</p>
+                                    </div>
+                                    <div class="it-time">
+                                        <span class="minutes">				
+                                        </span><br>	
+                                        <p>3 hours</p>
+                                    </div>
+                                    <div class="it-time">
+                                        <span class="seconds">							
+                                        </span><br>
+                                        <p>7 days</p>
+                                    </div>
+                                </div>	
+                            </div>
+                            <h3>Nofication me</h3>
+                            <form action="#">
+                                <input class="email" type="text" placeholder="Enter your email">
+                                <input class="redbtn" type="submit" placeholder="subscribe">
+                            </form>
+                        </div>
+                        <div class="col-md-6 col-sm-12 col-xs-12">
+                            <img class="cm-img" src="@/assets/images/uploads/cm-img.png" alt="">
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CommingSoon'
+}
+</script>
+
+<style>
+
+</style>
